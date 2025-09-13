f659ad45bc5e69310e88fb9c47469eff82ba7e7b
nitinmishra74 <fabnitin02@gmail.com>
2025-09-13 11:38:27 +0530

    Fix: removed submodule, added frontend as normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import TaskManager from './TaskManager.js'
+function App() {
+  return (
+    <div className="App">
+      < TaskManager />
+    </div>
+  );
+}
+
+export default App;
