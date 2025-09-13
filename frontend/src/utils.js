474d683023b7bde2cede6e152da66ec295389a10
Nitin Mishra <fabnitin02@gmail.com>
2025-09-13 12:01:13 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,4 +2,4 @@ import {toast} from 'react-toastify';
 export const notify = (message,type)=>{
     toast[type](message);
 }
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://mern-task-trackerapi.vercel.app';
